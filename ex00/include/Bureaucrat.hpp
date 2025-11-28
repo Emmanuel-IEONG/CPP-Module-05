00c169f2bd93ac0cca8c308605a50982755f79d1
Emmanuel Ieong <eieong@f2r11s16.clusters.42paris.fr>
2025-11-28 12:32:50 +0100

    feat: ex00 class + exceptions done, need tests

--- ex00/include/Bureaucrat.hpp
+++ ex00/include/Bureaucrat.hpp
@@ -6,30 +6,46 @@
 /*   By: eieong <eieong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 14:05:02 by eieong            #+#    #+#             */
-/*   Updated: 2025/11/21 14:12:23 by eieong           ###   ########.fr       */
+/*   Updated: 2025/11/28 12:06:35 by eieong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCRAT_HPP
 	#define BUREAUCRAT_HPP
 	#include <iostream>
+	#include <string>
 
 	class	Bureaucrat
 	{
 		private:
-			std::string	_name;
-			int			_grade;
+			const std::string	_name;
+			int					_grade;
 
 		public:
 			Bureaucrat();
+			Bureaucrat(const std::string, int grade);
 			Bureaucrat(Bureaucrat const & src);
 			~Bureaucrat();
 
-			Bureaucrat &	operator=(Bureaucrat const & rhs);
+			Bureaucrat	&operator=(Bureaucrat const & rhs);
 			std::string	getName() const;
-			int	getGrade() const;
-			void	increaseGrade();
-			void	decreaseGrade();
+			int			getGrade() const;
+			void		incrementGrade();
+			void		decrementGrade();
+			
+		class	GradeTooHighException : public std::exception
+		{
+			public:
+			virtual const char *what() const throw();
+		};
+		
+		class	GradeTooLowException : public std::exception
+		{
+			public:
+			virtual const char *what() const throw();
+		};
 	};
 
+	std::ostream	&operator<<(std::ostream &out, Bureaucrat *b);
+	
 #endif
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,8 +6,18 @@
 /*   By: eieong <eieong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 14:04:20 by eieong            #+#    #+#             */
-/*   Updated: 2025/11/21 14:04:36 by eieong           ###   ########.fr       */
+/*   Updated: 2025/11/28 12:31:25 by eieong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/Bureaucrat.hpp"
 
+int	main()
+{
+	Bureaucrat	*a = new Bureaucrat();
+	std::cout << a;
+	a->decrementGrade();
+	std::cout << a;
+	delete a;
+	return (0);
+}
